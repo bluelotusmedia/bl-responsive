10f55fb3784075a12a48ea348159ffa4a6daba65
bluelotusmedia <jacob@bluelotusmedia.com>
2016-04-29 13:28:14 -0600

    Modified footer menu, added copyrights, credits and changed some styles.

--- footer.php
+++ footer.php
@@ -1,7 +1,18 @@
 <footer class="footer" id="footer">
 <?php get_sidebar('footer'); ?>
-<hr>
-<?php wp_nav_menu(array('theme_location'=>'footer')); ?>
+<hr class="bl-divider" />
+<div class="footer-menu"><?php wp_nav_menu(array('theme_location'=>'footer')); ?></div>
+<div class="footer__credits-row">  
+        <div class="bl-copyright">
+        <?php echo "&copy;" . ' ' . date("Y") . ' '; bloginfo('name');?>. All rights reserved.
+        </div>
+        <div class="bl-websitecredit">
+        BL Responsive by <a href="http://bluelotusmedia.com">Blue Lotus Media</a>
+        </div>
+</div>
+
+<div class="bl-spacer"></div>
+
 </footer>
 	<?php wp_footer(); ?>
     	</div> <!-- container -->
